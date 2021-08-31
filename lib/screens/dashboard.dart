789d2095f13afae7e5d1d0ec253c8dad3fd92af2
brian <omondibrian392@gmail.com>
2021-08-31 08:38:42 +0300

    added welcome and dashboard Screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,8 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rada_egerton/constants.dart';
 
 class Dashboard extends StatelessWidget {
+  Widget dashBoardBuilder(BuildContext ctx, int index) {
+    return ListTile(
+      leading: SvgPicture.asset(
+        dashboardItems[index]['leadingIcon'],
+        width: 80,
+      ),
+      title: Text(dashboardItems[index]['title']),
+      subtitle: Text(dashboardItems[index]['subtitle']),
+      trailing: IconButton(
+        icon: Icon(Icons.arrow_forward_ios),
+        onPressed: ()=> Navigator.of(ctx).pushNamed(dashboardItems[index]['routeName']),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
+        title: Text('Rada DashBoard'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+            itemBuilder: dashBoardBuilder,
+            itemCount: dashboardItems.length,
+          ),
+        ),
+      ),
+    );
   }
 }
+
+List dashboardItems = [
+  {
+    'title': 'Mentorship',
+    'subtitle': 'Mentorship Program',
+    'leadingIcon': 'assets/mentor.svg',
+    'routeName':AppRoutes.mentorship
+  },
+  {
+    'title': 'Help',
+    'subtitle': 'Location and Contact',
+    'leadingIcon': 'assets/help.svg',
+    'routeName':AppRoutes.help
+  },
+  {
+    'title': 'Information',
+    'subtitle': 'Mentorship Program',
+    'leadingIcon': 'assets/information.svg',
+    'routeName':AppRoutes.information
+  },
+  {
+    'title': 'Student Forums',
+    'subtitle': 'Mentorship program',
+    'leadingIcon': 'assets/forum.svg',
+    'routeName':AppRoutes.forum
+  },
+  {
+    'title': 'Students Counseling',
+    'subtitle': 'Mentorship program',
+    'leadingIcon': 'assets/counseling.svg',
+    'routeName':AppRoutes.counseling
+  },
+  {
+    'title': 'Group Counseling',
+    'subtitle': 'Mentorship program',
+    'leadingIcon': 'assets/counseling.svg',
+    'routeName':AppRoutes.counseling
+  },
+  {
+    'title': 'Notification',
+    'subtitle': 'Mentorship program',
+    'leadingIcon': 'assets/bell.svg',
+    'routeName':AppRoutes.notification
+  },
+];
--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,12 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../constants.dart';
+import '../widgets/RadaButton.dart';
 
 class Welcome extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: SvgPicture.asset('assets/curve_left.svg'),
+          ),
+          Center(
+              child: SvgPicture.asset(
+            'assets/help.svg',
+            width: 200,
+          )),
+          Center(
+              child: Text('Welcome to Egerton University \n rada Application')),
+          Center(
+            child: RadaButton(
+                handleClick: () {
+                  Navigator.of(context).pushNamed(AppRoutes.login);
+                },
+                title: 'LogIn',
+                fill: true),
+          ),
+          Center(
+            child: RadaButton(
+                handleClick: () {
+                  Navigator.of(context).pushNamed(AppRoutes.dashboard);
+                },
+                title: 'Register',
+                fill: false),
+          ),
+        ],
+      ),
     );
   }
 }
