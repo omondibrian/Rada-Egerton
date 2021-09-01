6c6a56a2df477eab22c062ddecb06b3df7a403d0
brian <omondibrian392@gmail.com>
2021-09-01 23:43:44 +0300

    fixing maps

--- lib/screens/helpScreenTab/locationTab.dart
+++ lib/screens/helpScreenTab/locationTab.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationTab extends StatelessWidget {
-  const LocationTab({Key? key}) : super(key: key);
-  static final CameraPosition _initialPosition = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(-0.28333 ,36.06667),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  LocationTab({Key? key}) : super(key: key);
+  late final GoogleMapController mapController;
+  final CameraPosition _initialPosition = CameraPosition(
+    bearing: 90,
+    target: const LatLng(20.5937, 78.9629),
+    tilt: 45,
+    zoom: 4,
+  );
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-      initialCameraPosition: _initialPosition,
-      mapType: MapType.hybrid,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: this._initialPosition,
     );
   }
 }
