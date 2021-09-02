c3029add216bc276351a772a1759414921eb60b5
brian <omondibrian392@gmail.com>
2021-09-02 16:24:58 +0300

    added chat screen

--- lib/screens/forum.dart
+++ lib/screens/forum.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rada_egerton/widgets/AppBar.dart';
+import 'chat/chat.dart';
 
 class Forum extends StatelessWidget {
   final List _forums = [
@@ -16,16 +18,27 @@ class Forum extends StatelessWidget {
     }
   ];
   Widget forumBuilder(BuildContext context, int index) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: AssetImage(
-          this._forums[index]['imageUrl'],
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatScreen(
+            title: this._forums[index]['title'],
+            imgUrl: this._forums[index]['imageUrl'],
+          ),
         ),
       ),
-      title: Text(this._forums[index]['title'],
-          style: Theme.of(context).textTheme.headline1),
-      subtitle: Text('hello there say something',
-          style: Theme.of(context).textTheme.bodyText1),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: AssetImage(
+            this._forums[index]['imageUrl'],
+          ),
+        ),
+        title: Text(this._forums[index]['title'],
+            style: Theme.of(context).textTheme.headline1),
+        subtitle: Text('hello there say something',
+            style: Theme.of(context).textTheme.bodyText1),
+      ),
     );
   }
 
@@ -40,6 +53,31 @@ class Forum extends StatelessWidget {
           itemBuilder: forumBuilder,
           itemCount: this._forums.length,
         ),
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  final String title;
+  final String imgUrl;
+
+  ChatScreen({
+    Key? key,
+    required this.title,
+    required this.imgUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          CustomAppBar(title: this.title, imgUrl: this.imgUrl),
+          Chat(
+            currentUserName: 'jonathan',
+          )
+        ],
       ),
     );
   }
