a25ef7ade8cc1b4390e82e3478e5f71972766642
Ashisoma <okalionesmus@gmail.com>
2021-09-03 00:20:26 +0300

    add the profile screen, methods to be implimented

---
+++ lib/screens/profile.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  Widget textField(@required String hintTextfield) {
+    return Material(
+        elevation: 4,
+        shadowColor: Colors.grey,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: TextField(
+          decoration: InputDecoration(
+              hintText: hintTextfield,
+              hintStyle: TextStyle(
+                letterSpacing: 2,
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+              ),
+              fillColor: Colors.white30,
+              filled: true,
+              border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10.0))),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.green,
+        // impliment the revert button to prev screen
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 450,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  //TODO: use the data here form the data base
+                  children: [
+                    textField('UserName'),
+                    textField('University'),
+                    textField('Campus'),
+                    textField('Sex'),
+                    Container(
+                        height: 55,
+                        width: double.infinity,
+                        child: RaisedButton(
+                            // TODO: create the update password function
+                            onPressed: () {},
+                            color: Colors.redAccent,
+                            child: Center(
+                                child: Text("Change password",
+                                    style: TextStyle(
+                                        fontSize: 23, color: Colors.white)))))
+                  ],
+                ),
+              )
+            ],
+          ),
+          // this is the custom shade behind the image
+          CustomPaint(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            painter: HeaderCurved(),
+          ),
+          CustomPaint(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            painter: HeaderCurved(),
+          ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 35,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ),
+              // image design form here
+              // TODO: add an image in the projo or use the one available
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  image: DecorationImage(
+                    image: AssetImage('image/profile.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 270, left: 184),
+            child: CircleAvatar(
+              backgroundColor: Colors.black54,
+              child: IconButton(
+                  icon: Icon(Icons.edit, color: Colors.white), onPressed: null),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderCurved extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.green;
+    Path path = Path()
+      ..relativeLineTo(0, 150)
+      ..quadraticBezierTo(size.width / 2, 225, size.width / 2, 150.0)
+      ..relativeLineTo(0, -150)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
