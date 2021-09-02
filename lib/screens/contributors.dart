34e257f7b183adc352f1a1a40230bf5d36abba2e
Ashisoma <okalionesmus@gmail.com>
2021-09-03 01:25:46 +0300

    ill do it tomorrow

--- lib/screens/contributors.dart
+++ lib/screens/contributors.dart
@@ -5,7 +5,23 @@ class ContributorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.green,
+        // impliment the revert button to prev screen
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+      ),
+      body: Container(
+        child: Row(
+          // TODO: Design the images and add list tiles of the contributors
+          children: [
+            Image.asset('assets/bell.svg'),
+            Image.asset('assets/bell.svg'),
+            Image.asset('assets/bell.svg'),
+          ],
+        ),
+      ),
+    );
   }
-  }
+}
