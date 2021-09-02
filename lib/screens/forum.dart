a5e345e6fb2675de54399c14565d97a0d0b22e12
brian <omondibrian392@gmail.com>
2021-09-02 15:33:59 +0300

    initial forum page

--- lib/screens/forum.dart
+++ lib/screens/forum.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 
 class Forum extends StatelessWidget {
+  final List _forums = [
+    {
+      'title': 'Covid 19',
+      'imageUrl':
+          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfxGfUIe6333mIDDyqqnOgHaFb%26pid%3DApi&f=1',
+    }
+  ];
+  Widget forumBuilder(BuildContext context, int index) {
+    return ListTile(
+      leading: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              this._forums[index]['imageUrl'],
+            ),
+          ),
+        ),
+      ),
+      title: Text(this._forums[index]['title'],
+          style: Theme.of(context).textTheme.headline6),
+      subtitle: Text('hello there say something',
+          style: Theme.of(context).textTheme.headline6),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemBuilder: forumBuilder,
+      itemCount: this._forums.length,
+    );
   }
 }
