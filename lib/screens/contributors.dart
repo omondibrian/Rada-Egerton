e3aaa4d38a9dc77ab2c7292e1f14b6ef14f4d479
Ashisoma <okalionesmus@gmail.com>
2021-09-03 14:25:18 +0300

    list tile for the contributors

--- lib/screens/contributors.dart
+++ lib/screens/contributors.dart
@@ -1,26 +1,60 @@
 import 'package:flutter/material.dart';
 
 class ContributorScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.green,
         // impliment the revert button to prev screen
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
       ),
-      body: Container(
-        child: Row(
-          // TODO: Design the images and add list tiles of the contributors
-          children: [
-            Image.asset('assets/bell.svg'),
-            Image.asset('assets/bell.svg'),
-            Image.asset('assets/bell.svg'),
-          ],
-        ),
+      body: Stack(
+        children: [
+          Row(
+            children: [
+               Image.asset(''),
+               Image.asset(''),
+               Image.asset(''),
+              ],
+          ),
+          Column(
+            children: const <Widget>[
+                Expanded(
+                  child:ListTile(
+                    title: Text('Brian'),
+                    subtitle: Text('Software developer'),
+                    //isThreeLine: Text(),
+                  ),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text('Moses'),
+                    subtitle: Text('Software developer'),
+                  ),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text('Jonathan'),
+                    subtitle: Text('Software developer'),
+                  ),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text('Onesmus'),
+                    subtitle: Text('Software developer'),
+                  ),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text('Said'),
+                    subtitle: Text('Software developer'),
+                  ),
+                ),
+            ]
+          )
+        ],
       ),
     );
   }
