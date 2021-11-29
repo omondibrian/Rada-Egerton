5bd5f2b4b06278ac9f590c742ada747628d8cfee
Brian Omondi <omondibrian392@gmail.com>
2021-11-29 15:38:50 +0300

    refactor user profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -10,9 +10,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:rada_egerton/screens/widgets/ProfileHeader.dart';
 
-
-
-
 // ignore: must_be_immutable
 class ProfileScreen extends StatefulWidget {
   @override
@@ -90,135 +87,138 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            ProfileHeader(
-              avatar: CachedNetworkImageProvider(
-                'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-              ),
-              coverImage: AssetImage('assets/android-icon-192x192.png'),
-              title: "User Name", //TODO: fetch username
-
-              actions: <Widget>[
-                MaterialButton(
-                  color: Colors.white,
-                  shape: CircleBorder(side: BorderSide.none),
-                  elevation: 0,
-                  child: Icon(
-                    Icons.edit,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  onPressed: () {
-                    setState(
-                      () {
-                        userInfoControllerStatus = !userInfoControllerStatus;
-                      },
-                    );
-                    //TODO: implement edit details function
-                  },
-                )
-              ],
-            ),
-            const SizedBox(height: 10.0),
-            // UserInfo(),
-
-            //TODO: insert userinfo stless widget
-            Container(
-              padding: EdgeInsets.all(10),
+      body: widget.userProfile!.id.isEmpty
+          ? Text('loading')
+          : SingleChildScrollView(
               child: Column(
                 children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(
-                      left: 8.0,
+                  ProfileHeader(
+                    avatar: CachedNetworkImageProvider(
+                      'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
                     ),
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Profile Information",
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.left,
+                    coverImage: AssetImage('assets/android-icon-192x192.png'),
+                    title: "User Name", //TODO: fetch username
+
+                    actions: <Widget>[
+                      MaterialButton(
+                        color: Colors.white,
+                        shape: CircleBorder(side: BorderSide.none),
+                        elevation: 0,
+                        child: Icon(
+                          Icons.edit,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          setState(
+                            () {
+                              userInfoControllerStatus =
+                                  !userInfoControllerStatus;
+                            },
+                          );
+                          //TODO: implement edit details function
+                        },
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10.0),
+                  // UserInfo(),
+
+                  //TODO: insert userinfo stless widget
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(
+                            left: 8.0,
+                          ),
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Profile Information",
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Card(
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            padding: EdgeInsets.all(15),
+                            child: Form(
+                              //TODO supply form key
+                              child: Column(
+                                children: <Widget>[
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      hintText: "Enter user name",
+                                    ),
+                                    enabled: !userInfoControllerStatus,
+                                    controller: _lastNameController,
+                                    validator: RequiredValidator(
+                                        errorText: "Required"),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      hintText: "Enter email address",
+                                    ),
+                                    enabled: !userInfoControllerStatus,
+                                    controller: _emailController,
+                                    validator: RequiredValidator(
+                                        errorText: "Required"),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      hintText: "Enter gender",
+                                    ),
+                                    enabled: !userInfoControllerStatus,
+                                    controller: _genderController,
+                                    validator: RequiredValidator(
+                                        errorText: "Required"),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      hintText: "Enter date of birth",
+                                    ),
+                                    enabled: !userInfoControllerStatus,
+                                    controller: _dateOfBirthController,
+                                    validator: RequiredValidator(
+                                        errorText: "Required"),
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      hintText: "Enter phone number",
+                                    ),
+                                    enabled: !userInfoControllerStatus,
+                                    controller: _phoneNumberController,
+                                    validator: RequiredValidator(
+                                        errorText: "Required"),
+                                  ),
+                                  ListTile(
+                                    leading: Icon(Icons.account_box_outlined),
+                                    title: Text("Account status"),
+                                    subtitle: Text(
+                                        "${widget.userProfile?.email}"), //TODO fetch user account status
+                                  ),
+                                  ListTile(
+                                    leading: Icon(Icons.calendar_today),
+                                    title: Text("Date joined"),
+                                    subtitle: Text(
+                                        "${widget.userProfile?.email}"), //TODO fetch the date the user joined
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  Card(
-                    child: Container(
-                      alignment: Alignment.topLeft,
-                      padding: EdgeInsets.all(15),
-                      child: Form(
-                        //TODO supply form key
-                        child: Column(
-                          children: <Widget>[
-                            TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Enter user name",
-                              ),
-                              enabled: !userInfoControllerStatus,
-                              controller: _lastNameController,
-                              validator:
-                                  RequiredValidator(errorText: "Required"),
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Enter email address",
-                              ),
-                              enabled: !userInfoControllerStatus,
-                              controller: _emailController,
-                              validator:
-                                  RequiredValidator(errorText: "Required"),
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Enter gender",
-                              ),
-                              enabled: !userInfoControllerStatus,
-                              controller: _genderController,
-                              validator:
-                                  RequiredValidator(errorText: "Required"),
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Enter date of birth",
-                              ),
-                              enabled: !userInfoControllerStatus,
-                              controller: _dateOfBirthController,
-                              validator:
-                                  RequiredValidator(errorText: "Required"),
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "Enter phone number",
-                              ),
-                              enabled: !userInfoControllerStatus,
-                              controller: _phoneNumberController,
-                              validator:
-                                  RequiredValidator(errorText: "Required"),
-                            ),
-                            ListTile(
-                              leading: Icon(Icons.account_box_outlined),
-                              title: Text("Account status"),
-                              subtitle: Text(
-                                  "${widget.userProfile?.email}"), //TODO fetch user account status
-                            ),
-                            ListTile(
-                              leading: Icon(Icons.calendar_today),
-                              title: Text("Date joined"),
-                              subtitle: Text(
-                                  "${widget.userProfile?.email}"), //TODO fetch the date the user joined
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
                 ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
