7753f3e2d786982b0ffd77881385fd4f05ccbb6b
Brian Omondi <omondibrian392@gmail.com>
2021-12-03 16:09:07 +0300

    fixed merges

--- lib/screens/helpScreenTab/ContactTab.dart
+++ lib/screens/helpScreenTab/ContactTab.dart
@@ -1,34 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:rada_egerton/widgets/ContactCard.dart';
 import 'package:rada_egerton/entities/ContactsDto.dart';
-import 'package:rada_egerton/services/NewsAndLocation/main.dart';
-
-// ignore: must_be_immutable
-class ContactTab extends StatelessWidget {
-
- late  ContactsDto _contactList ;
-
-  ContactTab({Key? key}) : super(key: key){
-      initContacts();
-  }
-
-initContacts() async {
-  NewsAndLocationServiceProvider contactsProvider = NewsAndLocationServiceProvider();
- var result = await contactsProvider.fetchContacts();
- result!.fold((contacts) => this._contactList = contacts, (r) => print('error = $r'));
-}
-  
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: this._contactList.contacts.length,
-      itemBuilder: (BuildContext ctx, int index) => ContactCard(
-        title: this._contactList.contacts[index].name,
-        phoneNumber: this._contactList.contacts[index].phone,
-        email: this._contactList.contacts[index].email,
-        location: this._contactList.contacts[index].universityId,
-=======
 import 'package:rada_egerton/entities/locationDto.dart';
 import 'package:rada_egerton/services/NewsAndLocation/main.dart';
 
@@ -113,7 +84,6 @@ class _ContactTabState extends State<ContactTab> {
             ),
           ],
         ),
->>>>>>> main
       ),
     );
   }
