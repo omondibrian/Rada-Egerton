da8cab9e24802db0e94999cbdaeb5ae9d9d0a069
mozes_njoroge <njorogemosesngure@gmail.com>
2022-06-23 14:42:17 +0300

    news_location_service firebase crashlytics integration complete

--- lib/data/services/news_location_service.dart
+++ lib/data/services/news_location_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:rada_egerton/resources/config.dart';
 import 'package:rada_egerton/data/entities/NewsDTO.dart';
 import 'package:rada_egerton/data/entities/locationDto.dart';
@@ -10,22 +11,29 @@ class NewsAndLocationServiceProvider {
   final String _hostUrl = GlobalConfig.baseUrl;
   final Dio _httpClientConn = Dio();
   final _timeOut = 10000;
+  static final FirebaseCrashlytics _firebaseCrashlytics =
+      FirebaseCrashlytics.instance;
 
   Future<Either<List<News>, ErrorMessage>> fetchNews() async {
     try {
       String token = await ServiceUtility.getAuthToken() as String;
       final result = await _httpClientConn.get(
-            "$_hostUrl/api/v1/admin/news",
-            options: Options(
-              headers: {
-                'Authorization': token,
-              },
-              sendTimeout: _timeOut,
-            ),
-          );
+        "$_hostUrl/api/v1/admin/news",
+        options: Options(
+          headers: {
+            'Authorization': token,
+          },
+          sendTimeout: _timeOut,
+        ),
+      );
       Iterable l = result.data["news"];
       return Left(List<News>.from(l.map((j) => News.fromJson(j))));
-    } on DioError catch (e) {
+    } on DioError catch (e, stackTrace) {
+      _firebaseCrashlytics.recordError(
+        e,
+        stackTrace,
+        reason: 'Error while fetching news',
+      );
       return Right(
         ServiceUtility.handleDioExceptions(e),
       );
@@ -36,17 +44,21 @@ class NewsAndLocationServiceProvider {
     String? authtoken = await ServiceUtility.getAuthToken();
     try {
       final result = await _httpClientConn.get(
-            "$_hostUrl/api/v1/admin/location",
-            options: Options(
-                headers: {'Authorization': authtoken},
-                sendTimeout: _timeOut),
-          );
+        "$_hostUrl/api/v1/admin/location",
+        options: Options(
+            headers: {'Authorization': authtoken}, sendTimeout: _timeOut),
+      );
       return Left(
         LocationsDto.fromJson(
           result.data,
         ),
       );
-    } on DioError catch (e) {
+    } on DioError catch (e, stackTrace) {
+      _firebaseCrashlytics.recordError(
+        e,
+        stackTrace,
+        reason: 'Error while fetching location pins',
+      );
       return Right(
         ServiceUtility.handleDioExceptions(e),
       );
@@ -57,14 +69,18 @@ class NewsAndLocationServiceProvider {
     String? authtoken = await ServiceUtility.getAuthToken();
     try {
       final result = await _httpClientConn.get(
-            "$_hostUrl/api/v1/admin/contact",
-            options: Options(
-                headers: {'Authorization': authtoken},
-                sendTimeout: _timeOut),
-          );
+        "$_hostUrl/api/v1/admin/contact",
+        options: Options(
+            headers: {'Authorization': authtoken}, sendTimeout: _timeOut),
+      );
       Iterable l = result.data["contacts"];
       return Left(List<Contact>.from(l.map((j) => Contact.fromJson(j))));
-    } on DioError catch (e) {
+    } on DioError catch (e, stackTrace) {
+      _firebaseCrashlytics.recordError(
+        e,
+        stackTrace,
+        reason: 'Error while fetching contacts details',
+      );
       var errMsg = ServiceUtility.handleDioExceptions(e);
       return Right(errMsg);
     }
