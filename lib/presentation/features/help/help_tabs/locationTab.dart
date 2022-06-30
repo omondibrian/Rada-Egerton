bd6b9fb28ae5b6d0fbce720a7b3cf21dd58a754a
mozes_njoroge <njorogemosesngure@gmail.com>
2022-06-30 19:18:59 +0300

    Fisrt successful integration of location markers

--- lib/presentation/features/help/help_tabs/locationTab.dart
+++ lib/presentation/features/help/help_tabs/locationTab.dart
@@ -16,56 +16,95 @@ class LocationTab extends StatefulWidget {
   const LocationTab({Key? key}) : super(key: key);
 
   @override
-  State<LocationTab> createState() => MapSampleState();
+  State<LocationTab> createState() => _LocationTabState();
 }
 
-class MapSampleState extends State<LocationTab> {
+class _LocationTabState extends State<LocationTab> {
   final Completer<GoogleMapController> _controller = Completer();
-
-  static const CameraPosition _initialCameraPosition = CameraPosition(
-    tilt: 45,
-    target: LatLng(-0.36932651926935073, 35.9313568419356),
-    zoom: 20.0,
+  final marker = const Marker(
+    markerId: MarkerId('Counselling Dept.'),
+    position: LatLng(-0.36973278363331785, 35.93140758698434),
   );
 
-  static const CameraPosition _egertonntcc = CameraPosition(
-    bearing: 90,
-    target: LatLng(-0.28896128588051473, 36.05793424851361),
-    tilt: 45,
-    zoom: 20.0,
-  );
+  void _onMapCreated(GoogleMapController mapController) {
+    _controller.complete(mapController);
+  }
+
+  final markers = <String, Marker>{
+    'counselling_dept': const Marker(
+      markerId: MarkerId('Counselling Dept.'),
+      position: LatLng(-0.36973278363331785, 35.93140758698434),
+    ),
+    'ntcc': const Marker(
+      markerId: MarkerId('NTCC.'),
+      position: LatLng(-0.28896128588051473, 36.05793424851361),
+    )
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        zoomGesturesEnabled: true,
-        tiltGesturesEnabled: true,
-        buildingsEnabled: true,
-        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
-          Factory<OneSequenceGestureRecognizer>(
-            () => EagerGestureRecognizer(),
-          ),
-        },
-        initialCameraPosition: _initialCameraPosition,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _gotoNTCC,
-        label: const Text('NTCC'),
-        icon: const Icon(Icons.school_outlined),
-      ),
+    return GoogleMap(
+      initialCameraPosition: _initialCameraPosition,
+      onMapCreated: _onMapCreated,
+      markers: markers.values.toSet(),
     );
   }
-
-  Future<void> _gotoNTCC() async {
-    final GoogleMapController mapController = await _controller.future;
-    mapController.animateCamera(CameraUpdate.newCameraPosition(_egertonntcc));
-  }
 }
+
+// class LocationTab extends StatefulWidget {
+//   const LocationTab({Key? key}) : super(key: key);
+
+//   @override
+//   State<LocationTab> createState() => MapSampleState();
+// }
+
+// class MapSampleState extends State<LocationTab> {
+//   final Completer<GoogleMapController> _controller = Completer();
+
+const CameraPosition _initialCameraPosition = CameraPosition(
+  tilt: 45,
+  target: LatLng(-0.36932651926935073, 35.9313568419356),
+  zoom: 20.0,
+);
+
+//   static const CameraPosition _egertonntcc = CameraPosition(
+//     bearing: 90,
+//     target: LatLng(-0.28896128588051473, 36.05793424851361),
+//     tilt: 45,
+//     zoom: 20.0,
+//   );
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: GoogleMap(
+//         mapType: MapType.normal,
+//         zoomGesturesEnabled: true,
+//         tiltGesturesEnabled: true,
+//         buildingsEnabled: true,
+//         gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+//           Factory<OneSequenceGestureRecognizer>(
+//             () => EagerGestureRecognizer(),
+//           ),
+//         },
+//         initialCameraPosition: _initialCameraPosition,
+//         onMapCreated: (GoogleMapController controller) {
+//           _controller.complete(controller);
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton.extended(
+//         onPressed: _gotoNTCC,
+//         label: const Text('NTCC'),
+//         icon: const Icon(Icons.school_outlined),
+//       ),
+//     );
+//   }
+
+//   Future<void> _gotoNTCC() async {
+//     final GoogleMapController mapController = await _controller.future;
+//     mapController.animateCamera(CameraUpdate.newCameraPosition(_egertonntcc));
+//   }
+// }
 
 //Dean Of Sudent's location -0.36932651926935073, 35.9313568419356
 //Counselling's department -0.36973278363331785, 35.93140758698434
